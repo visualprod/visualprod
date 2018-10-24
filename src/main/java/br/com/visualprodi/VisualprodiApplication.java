b83eab689b2515b6be431817a67710ed0c0f1bc9
visualizaoproducao <visualizaoproducao@gmail.com>
2018-10-24 19:30:22 -0300

    Commit Inicial

---
+++ src/main/java/br/com/visualprodi/VisualprodiApplication.java
@@ -0,0 +1,12 @@
+package br.com.visualprodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualprodiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualprodiApplication.class, args);
+	}
+}
