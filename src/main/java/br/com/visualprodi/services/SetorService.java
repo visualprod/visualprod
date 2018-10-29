3bc646b1b06f26fc0c6af578d4489d6824f69d55
visualizaoproducao <visualizaoproducao@gmail.com>
2018-10-29 19:59:33 -0300

    Exemplo Logging

--- src/main/java/br/com/visualprodi/services/SetorService.java
+++ src/main/java/br/com/visualprodi/services/SetorService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -20,32 +22,48 @@ public class SetorService {
 	@Autowired
 	private SetorRepository repo;
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	public List<Setor> findAll() {
-		return repo.findAll();
+		logger.info("Chamada findAll");
+		List<Setor> findAll = repo.findAll();
+		logger.info("Retorno findAll - " + findAll);
+		return findAll;
 	}
 
 	public Setor find(Integer id) {
+		logger.info("Chamada find id " + id);
 		Optional<Setor> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Setor.class.getName()));
+		Setor retorno = obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Setor.class.getName()));
+		logger.info("Retorno find id " + id);
+		logger.info("Retorno find " + retorno);
+		return retorno;
 	}
 
 	@Transactional
 	public Setor insert(Setor obj) {
+		logger.info("Chamada insert " + obj);
 		obj.setId(null);
 		obj = repo.save(obj);
+		logger.info("Retorno insert " + obj);
 		return obj;
 	}
 
 	public Setor update(Setor obj) {
-		return repo.save(obj);
+		logger.info("Chamada update " + obj);
+		Setor save = repo.save(obj);
+		logger.info("Chamada update " + save);
+		return save;
 	}
 
 	public void delete(Integer id) {
+		logger.info("Chamada delete id " + id);
 		try {
 			this.find(id);
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
+			logger.info("Retorno delete id " + id);
+			logger.info("Retorno delete exception " + e.getMessage());
 			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
